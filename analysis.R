3805a0a7f85de3010d9992d5ca9e6ef7dcb774c3
amarov <boyko.amarov@gmail.com>
2018-10-25 17:38:51 +0300

    ,,,

--- analysis.R
+++ analysis.R
@@ -8,17 +8,37 @@ str(rawData)
 dat <- within(rawData, {
   dBirth <- as.Date(dBirth, format = '%m/%d/%Y')
   dDiagnosis <- as.Date(dDiagnosis, format = '%m/%d/%Y')
+  progression <- factor(progression)
   ageOver80 <- factor(age > 80)
+  sex <- factor(sex)
   alive <- alive == 1
   dDeath <- as.Date(Date.of.death, format = '%m/%d/%Y')
   timeToDeath <- ifelse(!is.na(dDeath), (dDeath - dDiagnosis) / 365, followup)
   death <- alive != 1
-  terapyChar <- ifelse(terapy == 1, 'Chemo', 'Mixed')
-  terapyF <- factor(terapyChar)
+  therapyChar <- ifelse(terapy == 1, 'Radiation', 'Chemo/mixed')
+  therapy <- factor(therapyChar)
   time <- ifelse(is.na(timeToProgression), followup, timeToProgression)
   time <- time / 12
   event <- !is.na(timeToProgression)
 })
+
+## Data description
+
+summary(dat)
+
+## Overall median survival time
+
+summary(surv_fit(Surv(time, event) ~ 1, data = dat))
+surv_fit(Surv(time, event) ~ 1, data = dat)
+
+summary(survfit(Surv(time, event) ~ 1, data = dat))
+
+## Kaplan Meier estimates of the survival probability
+fit <- survfit(Surv(time, event) ~ therapy, data = dat)
+ggsurvplot(fit, data = dat)
+
+xtabs(~ alive + progression, data = dat)
+
 
 summary(lm(age ~ terapy, data = dat))
 
